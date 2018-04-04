7b99d9019a7ce336ef05de94408feb287ae81e85
Arvind H Nair <arvindnair96@Terminator.localdomain>
2018-04-05 03:42:37 +0530

    Initial Commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+    <h1>About US</h1>
+    <p>This is the about page</p>
+@endsection
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+    <h1>Welcome to PCE Placement</h1>
+    <p>This is a sample application created using Laravel</p>
+@endsection
